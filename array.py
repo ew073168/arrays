f38fdbaa8bd332a453aca3cca2a67dd195b07bdd
ew073168 <71084783+ew073168@users.noreply.github.com>
2020-09-10 09:58:39 -0500

    Create array.py
    we did this in class

---
+++ array.py
@@ -0,0 +1,8 @@
+colors = ["Red","Yellow","Green","Blue","Orange"]
+print(colors)
+print(colors[1])
+colors[4] = "Purple"
+print(colors[4])
+colors.append("Orange")
+print(colors[1],"+",colors[3],"=",colors[2])
+print(colors[5])
